65d453867d1c4fdc038627889291163507902c49
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:29:41 -0600

    Updated ExpenseDAOIplementation

--- src/main/java/com/expense/manager/dao/ExpenseDAOImplementation.java
+++ src/main/java/com/expense/manager/dao/ExpenseDAOImplementation.java
@@ -1,4 +1,19 @@
 package com.expense.manager.dao;
 
-public class ExpenseDAOImplementation {
-}
+import com.expense.manager.model.Expense;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpenseDAOImplementation implements ExpenseDAO
+{
+    private Connection conn;
+
+    public ExpenseDAOImplementation(Connection conn)
+    {
+        this.conn = conn;
+    }
