ddae0a1385a1a35130aa90d79a6b7dc06d0aaaf5
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:33:09 -0600

    Updated DatabaseConnection

--- src/main/java/com/expense/manager/util/DatabaseConnection.java
+++ src/main/java/com/expense/manager/util/DatabaseConnection.java
@@ -1,4 +1,11 @@
 package com.expense.manager.util;
 
-public class DatabaseConnection {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection
+{
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/expense_db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "password";
