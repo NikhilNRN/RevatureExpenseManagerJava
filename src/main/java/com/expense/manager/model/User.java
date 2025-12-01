02ed2e4376548f9891bdef3970f48a83a41e9c9d
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:31:48 -0600

    Updated User

--- src/main/java/com/expense/manager/model/User.java
+++ src/main/java/com/expense/manager/model/User.java
@@ -2,5 +2,20 @@ package com.expense.manager.model;
 
 public class User
 {
-    // All the User class code
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(int id, String username, String password, String role)
+    {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getRole() { return role; }
 }
