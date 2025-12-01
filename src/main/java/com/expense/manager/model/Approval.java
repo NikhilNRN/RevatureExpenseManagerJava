d60134a604915d9ce09ee2f48c28afc10003c406
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:31:27 -0600

    Updated Approval

--- src/main/java/com/expense/manager/model/Approval.java
+++ src/main/java/com/expense/manager/model/Approval.java
@@ -1,4 +1,29 @@
 package com.expense.manager.model;
 
-public class Approval {
+public class Approval
+{
+    private int id;
+    private int expenseId;
+    private String status;
+    private Integer reviewerId;
+    private String comment;
+    private String reviewDate;
+
+    public Approval(int id, int expenseId, String status) {
+        this.id = id;
+        this.expenseId = expenseId;
+        this.status = status;
+    }
+
+    public int getId() { return id; }
+    public int getExpenseId() { return expenseId; }
+    public String getStatus() { return status; }
+    public Integer getReviewerId() { return reviewerId; }
+    public String getComment() { return comment; }
+    public String getReviewDate() { return reviewDate; }
+
+    public void setStatus(String status) { this.status = status; }
+    public void setReviewerId(Integer reviewerId) { this.reviewerId = reviewerId; }
+    public void setComment(String comment) { this.comment = comment; }
+    public void setReviewDate(String date) { this.reviewDate = date; }
 }
