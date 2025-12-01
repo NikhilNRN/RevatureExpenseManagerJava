bf089726193497cd98c7d7e84ec4b9b1daab0bf6
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:29:27 -0600

    Updated ExpenseDAO

--- src/main/java/com/expense/manager/dao/ExpenseDAO.java
+++ src/main/java/com/expense/manager/dao/ExpenseDAO.java
@@ -1,4 +1,14 @@
 package com.expense.manager.dao;
 
-public interface ExpenseDAO {
+import com.expense.manager.model.Expense;
+
+import java.util.List;
+
+public interface ExpenseDAO
+{
+    List<Expense> getPendingExpenses() throws Exception;
+    Expense getExpenseById(int id) throws Exception;
+    List<Expense> getExpensesByEmployee(String username) throws Exception;
+    List<Expense> getExpensesByDateRange(String startDate, String endDate) throws Exception;
+    List<Expense> getExpensesByStatus(String status) throws Exception;
 }
