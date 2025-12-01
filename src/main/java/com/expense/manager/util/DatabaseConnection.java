d75ea110a364ee1c67aff746c210e9c8eeb42dc8
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:33:15 -0600

    Updated DatabaseConnection

--- src/main/java/com/expense/manager/util/DatabaseConnection.java
+++ src/main/java/com/expense/manager/util/DatabaseConnection.java
@@ -9,3 +9,16 @@ public class DatabaseConnection
     private static final String DB_URL = "jdbc:mysql://localhost:3306/expense_db";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException
+    {
+        try
+        {
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e)
+        {
+            System.err.println("Database connection failed: " + e.getMessage());
+            throw e;
+        }
+    }
+}
