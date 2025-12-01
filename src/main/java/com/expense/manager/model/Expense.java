1279fd76e57ce59bd2206f51432143a4f1e86e9d
Nikhil <thedemo2146@gmail.com>
2025-12-01 00:31:36 -0600

    Updated Expense

--- src/main/java/com/expense/manager/model/Expense.java
+++ src/main/java/com/expense/manager/model/Expense.java
@@ -1,4 +1,19 @@
 package com.expense.manager.model;
 
-public class Expense {
-}
+public class Expense
+{
+    private int id;
+    private int userId;
+    private double amount;
+    private String description;
+    private String date;
+    private String employeeName;
+    private String status;
+
+    public Expense(int id, int userId, double amount, String description, String date) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
